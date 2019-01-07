cdbf78324ad9fca05f589563aeb2afa4c7e8ff88
preethi <preethi@preethi-PC>
2019-01-07 15:43:09 +0530

    First commit

---
+++ src/main/java/com/ind/lmc/LMCProject1/App.java
@@ -0,0 +1,15 @@
+package com.ind.lmc.LMCProject1;
+
+import java.util.Date;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World! Preethi "+new Date() );
+    }
+}
